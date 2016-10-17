b1b6a2ca95cb210af150a90f23812b28e20a3bf2
kn45 <381404825@qq.com>
2016-10-17 16:04:32 +0800

    fix a bug cz by wc -l diff btwn osx and linux

--- 2_Split.sh
+++ 2_Split.sh
@@ -19,27 +19,27 @@ valid_ratio=0.15
 # For regression task, random sampling
 rand_samp()
 {
-  all_cnt=`cat $data_src | wc -l`
+  all_cnt=`cat $data_src | wc -l | sed -e 's/^[ 	]*//'`
 
   # split to train_valid + test
   data_src_shuf=${data_src}.shuf
-  test_cnt=`echo | awk -v all=$all_cnt -v rate=$test_ratio 'BEGIN{print int(all*rate)}'`
-  trnvld_cnt=`echo | awk -v tst=$test_cnt -v all=$all_cnt 'BEGIN{print all-tst}'`
+  test_cnt=`echo | awk 'BEGIN{print int("'"$all_cnt"'"*"'"$test_ratio"'")}'`
+  trnvld_cnt=`echo ""| awk 'BEGIN{print "'"$all_cnt"'"-"'"$test_cnt"'"}'`
   cat $data_src | perl -MList::Util=shuffle -e'print shuffle<>' > $data_src_shuf
   head -n $trnvld_cnt $data_src_shuf | perl -MList::Util=shuffle -e'print shuffle<>' > $data_trnvld
   tail -n $test_cnt $data_src_shuf > $data_test
-  echo -e $all_cnt" = "$trnvld_cnt" + "$test_cnt"\tTest Ratio: "$test_ratio
+  echo -e "   (all)"$all_cnt"  = (trnvld)"$trnvld_cnt"  +  (test)"$test_cnt"\tTest Ratio: "$test_ratio
   rm -f $data_src_shuf
 
   # split train_valid to train + valid
   # for the training without cross-validation
-  valid_cnt=`echo | awk -v all=$trnvld_cnt -v rate=$valid_ratio 'BEGIN{print int(all*rate)}'`
-  train_cnt=`echo | awk -v valid=$valid_cnt -v all=$all_cnt 'BEGIN{print all-valid}'`
+  valid_cnt=`echo | awk 'BEGIN{print int("'"$trnvld_cnt"'"*"'"$valid_ratio"'")}'`
+  train_cnt=`echo | awk 'BEGIN{print "'"$all_cnt"'"-"'"$valid_cnt"'"}'`
   head -n $train_cnt $data_trnvld > $data_train
   tail -n $valid_cnt $data_trnvld > $data_valid
-  echo -e $trnvld_cnt" = "$train_cnt" + "$valid_cnt"\tValid Ratio: "$valid_ratio
+  echo -e "(trnvld)"$trnvld_cnt"  =  (train)"$train_cnt"  + (valid)"$valid_cnt"\tValid Ratio: "$valid_ratio
 }
-#rand_samp
+rand_samp
 
 
 # For classification task, stratified sampling
@@ -48,4 +48,4 @@ strf_samp()
   python 2_Split.py $data_src $data_trnvld $data_test $test_ratio
   python 2_Split.py $data_trnvld $data_train $data_valid $valid_ratio
 }
-strf_samp
+#strf_samp
