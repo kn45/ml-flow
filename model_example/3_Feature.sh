f736336cc29da9c7c896eb9667816226a7f57ade
kn45 <381404825@qq.com>
2017-07-03 12:04:50 +0800

    fix relative path

--- model_example/3_Feature.sh
+++ model_example/3_Feature.sh
@@ -3,15 +3,15 @@
 # Build feature using various ways from tsv file
 # tsv -> feature
 
-data_trnvld=data_train/data_trnvld.tsv
-data_train=data_train/data_train.tsv
-data_valid=data_train/data_valid.tsv
-data_test=data_test/data_test.tsv
+data_trnvld=../data_train/data_trnvld.tsv
+data_train=../data_train/data_train.tsv
+data_valid=../data_train/data_valid.tsv
+data_test=../data_test/data_test.tsv
 
-feat_trnvld=data_train/trnvld_feature.tsv
-feat_train=data_train/train_feature.tsv
-feat_valid=data_train/valid_feature.tsv
-feat_test=data_test/test_feature.tsv
+feat_trnvld=feat_train/trnvld_feature.tsv
+feat_train=feat_train/train_feature.tsv
+feat_valid=feat_train/valid_feature.tsv
+feat_test=feat_test/test_feature.tsv
 
 python 3_Feature.py $data_train $feat_train
 python 3_Feature.py $data_valid $feat_valid
